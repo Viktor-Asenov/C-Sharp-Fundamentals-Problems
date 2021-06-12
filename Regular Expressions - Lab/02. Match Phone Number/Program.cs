f5e618c91cf3a6458bbfb19f40376cecbe8f562a
Viktor-Asenov <79524317+Viktor-Asenov@users.noreply.github.com>
2021-06-12 16:42:44 +0300

    Solutions of Regular Expressions Problems - Lab

---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\+359(\s|-)([2])\1(\d{3})\1(\d{4})\b";
+
+            string phoneNumbers = Console.ReadLine();
+
+            MatchCollection phoneMatches = Regex.Matches(phoneNumbers, regex);
+
+            var matchedPhones = phoneMatches.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
---
+++ Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})([-.|\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            var datesString = Console.ReadLine();
+
+            var dates = Regex.Matches(datesString, pattern);
+
+            foreach (Match date in dates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
